e41ccd3a18c9b10d4bbbe0e9544af76f4dd2b1a1
Anand Bharti <banand@athenahealth.com>
2020-04-21 15:59:11 +0530

    state, callbacks

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,21 +3,25 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
+import Message from './components/Message'
+import Counter from './components/Counter'
 
 export default class App extends Component  {
   render() {
   return (
     <div className="App">
+    <Counter />
+      {/* <Message /> */}
           {/* Functions */}
-          <Greet name = "Bruce" heroname = "BatMan">
+          {/* <Greet name = "Bruce" heroname = "BatMan">
             <p>This is children props </p>
           </Greet>
           <Greet name = "Clark" heroname = "SuperMan"/>
-          <Greet name = "Steve" heroname = "Captain America"/>
+          <Greet name = "Steve" heroname = "Captain America"/> */}
           {/* classes */}
-          <Welcome name = "Bruce" heroname = "BatMan"/>
+          {/* <Welcome name = "Bruce" heroname = "BatMan"/>
           <Welcome name = "Clark" heroname = "SuperMan"/>
-          <Welcome name = "Steve" heroname = "Captain America"/>
+          <Welcome name = "Steve" heroname = "Captain America"/> */}
           {/* <Hello /> */}
     </div>
   );
