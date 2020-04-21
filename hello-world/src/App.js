9a51340016c40c908927f81af789c316f34ff95e
Anand Bharti <banand@athenahealth.com>
2020-04-21 15:16:55 +0530

    props

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
+import Hello from './components/Hello'
 
 export default class App extends Component  {
   render() {
   return (
     <div className="App">
-          <Greet />
-          <Welcome />
+          {/* Functions */}
+          <Greet name = "Bruce" heroname = "BatMan">
+            <p>This is children props </p>
+          </Greet>
+          <Greet name = "Clark" heroname = "SuperMan"/>
+          <Greet name = "Steve" heroname = "Captain America"/>
+          {/* classes */}
+          <Welcome name = "Bruce" heroname = "BatMan"/>
+          <Welcome name = "Clark" heroname = "SuperMan"/>
+          <Welcome name = "Steve" heroname = "Captain America"/>
+          {/* <Hello /> */}
     </div>
   );
   }
--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -3,6 +3,15 @@ import React from 'react'
 // function Greet() {
 //     return <h1>Hello Anand!</h1>
 // } 
-const Greet = () => <h1>Hello Anand!</h1>
+const Greet = (props) => { 
+    console.log(props)
+
+    return (
+        <div>
+    <h1>Hello {props.name} or {props.heroname}!</h1>
+    <h2>{props.children}</h2>
+    </div>
+    )
+}
 
 export default Greet
